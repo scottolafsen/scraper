cf1f723125a812cf5dc484ba43685ae5b5eef04e
Scott <scott.olafsen@gmail.com>
2019-03-27 16:18:33 -0600

    second draft

--- public/app.js
+++ public/app.js
@@ -1,25 +1,23 @@
-// Grab the articles as a json
-
-function loadArticles(){
-$.getJSON("/articles", function (data) {
-  $("#articles").empty();
-  
-  for (var i = 0; i < data.length; i++) {
-    if (data[i].favorite === false){
-    $("#articles").append("<li class=list-group-item data-id=" + data[i]._id + ">" + data[i].title + "<br />" + data[i].link + "<btn type=button class= 'btn btn-success saveArt' data-id=" + data[i]._id + ">Save Article</btn> </li>");
-  }
-}
-});
+// Loads unsaved articles on page
+function loadArticles() {
+  $.getJSON("/articles", function (data) {
+    $("#articles").empty();
+    for (var i = data.length - 1; i >= 0; i--) {
+      if (data[i].favorite === false) {
+        $("#articles").append("<li class=list-group-item data-id=" + data[i]._id + "><img src=" + data[i].img + " class=img-fluid alt=Responsive image><a href=" + data[i].link + " class=btn btn-primary stretched-link>" + data[i].title + "</a><btn type=button class= 'btn btn-success saveArt' data-id=" + data[i]._id + ">Save Article</btn> </li>");
+      }
+    }
+  });
 }
 
 $(document).on("click", ".saveArt", function () {
-  // Empty the notes from the note section
   var thisId = $(this).attr("data-id");
   $.ajax({
     method: "POST",
     url: "/saved/" + thisId
   }).then(function () {
     loadArticles();
+    console.log("favorited");
   })
 });
 
@@ -29,6 +27,37 @@ $(document).on("click", ".clear", function () {
     url: "/delete"
   }).then(function () {
     loadArticles();
+    console.log("deleted");
+  })
+});
+
+$(document).on("click", "#scrape-unoffical", function () {
+  $.ajax({
+    method: "GET",
+    url: "/scrape/unoffical"
+  }).then(function () {
+    loadArticles();
+    console.log("scraped");
+  })
+});
+
+$(document).on("click", "#scrape-snowbrains", function () {
+  $.ajax({
+    method: "GET",
+    url: "/scrape/snowbrains"
+  }).then(function () {
+    loadArticles();
+    console.log("scraped");
+  })
+});
+
+$(document).on("click", "#scrape-teton", function () {
+  $.ajax({
+    method: "GET",
+    url: "/scrape/tetongravity"
+  }).then(function () {
+    loadArticles();
+    console.log("scraped");
   })
 });
 
--- public/saved.js
+++ public/saved.js
@@ -1,71 +1,70 @@
-$.getJSON("/articles", function (data) {
-  console.log(data)
-  $("#articles").empty();
-  for (var i = 0; i < data.length; i++) {
-    // $("#articles").empty();
-    if (data[i].favorite === true) {
-      // Display the apropos information on the page
-      $("#articles").append("<li class=list-group-item data-id=" + data[i]._id + ">" + data[i].title + "<br />" + data[i].link + "<btn type=button class= 'btn btn-danger delete' data-id=" + data[i]._id + ">Delete</btn>" +
-        "<br /><form> <div class=form-group> <label for=title" + data[i]._id + "> Title </label><input type=text class=form-control id=title" + data[i]._id + "><label for=body" + data[i]._id + "> Write Note </label><input type=text class=form-control id=body" + data[i]._id + "><btn type=button class= 'btn btn-success note' data-id=" + data[i]._id + ">Write Note</btn></div> </form></li>");
+// Gets Articles from DB and prints to page
+function loadPage() {
+  $.getJSON("/articles", function (data) {
+    console.log(data)
+    $("#articles").empty();
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].favorite === true) {
+        $("#articles").append("<li class=list-group-item data-id=" + data[i]._id + "><img src=" + data[i].img + " class=img-fluid alt=Responsive image><a href=" + data[i].link + " " + "class=btn btn-primary stretched-link>" + data[i].title + "</a><btn type=button class= 'btn btn-danger delete' data-id=" + data[i]._id + ">Delete</btn>" +
+          "<br /><form> <div class=form-group> <label for=title" + data[i]._id + "> Title </label><input type=text class=form-control id=title" + data[i]._id + "><label for=body" + data[i]._id + "> Write Note </label><input type=text class=form-control id=body" + data[i]._id + "><btn type=button class= 'btn btn-success note' data-id=" + data[i]._id + ">Write Note</btn></div> </form>");
+      }
+      if (data[i].note) {
+        $("#articles").append("<li class=list-group-item>" + data[i].note.title + " ||  " + data[i].note.body + "    <btn type=button class= 'btn btn-danger deletenote' data-id=" + data[i].note._id + ">x</btn></li>");
+      }  $("#articles").append("</li>");
     }
-    if (data[i].note) {
-      // Place the title of the note in the title input
-      // Place the body of the note in the body textarea
-      $("#articles").append("<li class=list-group-item>" + data[i].note.title + "||" + data[i].note.body + "</li>");
-    }
-  }
-});
+  })
+};
 
 // When you click the savenote button
 $(document).on("click", ".note", function () {
-  // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
   var title = $("#title" + thisId).val();
   var body = $("#body" + thisId).val();
   console.log(title, body);
-
-  // Run a POST request to change the note, using what's entered in the inputs
+  // Run a POST request to change the note, or create note
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
-      // Value taken from title input
       title: title,
-      // Value taken from note textarea
       body: body
     }
   })
-    // With that done
     .then(function (data) {
-      // Log the response
       console.log(data);
-      // Empty the notes section
+      loadPage();
 
     });
-  // Also, remove the values entered in the input and textarea for note entry
   $("#title" + thisId).val("");
   $("#body" + thisId).val("");
 });
 
 // Delete Saved Article
 $(document).on("click", ".delete", function () {
-  // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
-  // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "DELETE",
     url: "/delete/" + thisId,
   })
-    // With that done
     .then(function () {
-      // Log the response
+      loadPage();
       console.log("deleted");
-      // Empty the notes section
     });
 });
 
+$(document).on("click", ".deletenote", function () {
+  var thisId = $(this).attr("data-id");
+  $.ajax({
+    method: "DELETE",
+    url: "/delete/note/" + thisId,
+  })
+    .then(function () {
+      loadPage();
+      console.log("deleted");
+    });
+});
 
+loadPage();
 
-//   
 
 
